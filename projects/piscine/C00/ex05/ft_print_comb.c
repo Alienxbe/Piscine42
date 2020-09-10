e0729194fdccbbdaec06bb34231b69e2eddf712b
Martin Ykman <mykman@shi-r3-p7.s19.be>
2020-09-10 15:48:14 +0200

    Upload first part of C00 (ex 00 -> 05)

---
+++ projects/piscine/C00/ex05/ft_print_comb.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mykman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/10 13:19:37 by mykman            #+#    #+#             */
+/*   Updated: 2020/09/10 15:38:22 by mykman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_show_number(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a != '7' || b != '8' || c != '9')
+	{
+		write(1, ", ", 2);
+	}
+	else
+	{
+		write(1, "\n", 1);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char i;
+	char j;
+	char k;
+
+	i = '0';
+	while (i <= '9')
+	{
+		j = i + 1;
+		while (j <= '9')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				ft_show_number(i, j, k);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
